05c0e11030af63057a406f665c24ffc1adfb19db
Nicola Buckhurst <nicola@superputerpro.home>
2018-07-28 22:02:09 +0100

    render form to enter CLA agreement details

--- routes/CLA.js
+++ routes/CLA.js
@@ -5,19 +5,53 @@ var expressSession = require('express-session')
 
 var databaseStore = require("../models/DatabaseStore")
 
+var showdown = require('showdown')
+
+var claContents = require("../test/data/Apachev2.0")
+
+var fs = require('fs');
+var path = require('path')
+
+var converter = new showdown.Converter()
+
 router.get("/", function(req, res, next){
   let loggedIn, profilepicture
-
+  //if user is alread logged in, set logged in to true and profilepicture so that navbar displays correctly
   if (req.user != null){
     loggedIn = true;
     profilePicture = req.user["githubPicture"]
   } else {
+    //if user not logged in then redirect to login page
     loggedIn = false;
-    profilePicture = "#"
+    profilePicture = "#";
+    res.redirect('/login/github')
   }
 
+  let claContentsParsed ={}
 
-  res.render('CLA', {"title":"Sign CLA", "loggedIn":loggedIn, "profilePicture":profilePicture})
+  let textPath = path.join(__dirname,'/../test/data/'+claContents["text"])
+
+  //read the text conent of the CLA asynchronously as a utf8 encoded string and the pas
+  //data into callbackURL so that the markdown text gets parsed to html before being sent to
+  //ejs template
+  fs.readFile(textPath,'utf8',function(err, data){
+    for(property in claContents){
+      if(property == "text"){
+        claContentsParsed["text"] = converter.makeHtml(data)
+      } else {
+        claContentsParsed[property]=claContents[property]
+      }
+    }
+    res.render('CLA', {"title":"Sign CLA", "loggedIn":loggedIn, "profilePicture":profilePicture, "claContents":claContentsParsed})
+  })
+
+})
+
+
+router.post("/", function(req, res, next){
+  console.log(JSON.stringify(req.body))
+  res.redirect("/")
+
 })
 
 module.exports=router
