644eb1f57e8f9d85a8eac4f133de5a47d3ee04f2
Nicola Buckhurst <nicola@superputerpro.local>
2018-10-17 21:06:48 +0100

    change config so all env variable config is run as a script from npm

--- config.js
+++
@@ -1,8 +0,0 @@
-process.env["REDIS_DEV_URL"] = "redis://localhost:6379";
-process.env["REDIS_TEST_URL"] = "redis://localhost:6380";
-
-if(process.env.NODE_ENV == "dev"){
-  process.env["REDIS_URL"] = process.env.REDIS_DEV_URL;
-} else {
-  process.env["REDIS_URL"] = process.env.REDIS_TEST_URL;
-}
