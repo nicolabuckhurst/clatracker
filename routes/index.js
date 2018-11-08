38ef774a5a55864a6bd3d2b6eacf2d1532fed03f
Nicola Buckhurst <nicola@superputerpro.local>
2018-11-08 14:23:41 +0000

    tidied up code

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,9 @@ var databaseStore = require('../models/DatabaseStore');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  let loggedIn, profilepicture
+  let loggedIn, profilePicture
   let claList = {}
+  let admin
 
   //set redirect url in session object to this route...if uses chooses to click login from
   //this page they will be rerouted back here after they have logged in
@@ -15,18 +16,18 @@ router.get('/', function(req, res, next) {
   //if user is logged in set admin status in session and retrieve a list of signed CLA's for homepage
   if (req.user != null){
     //check if user is admin and if they are store this information in session
-    databaseStore.checkAdminStatus(req.user["id"])
+    return databaseStore.checkAdminStatus(req.user["id"])
     .then(function(adminStatus){
-      let admin=adminStatus;
+      admin=adminStatus;
 
       loggedIn = true;
       profilePicture = req.user["githubPicture"]
 
       return databaseStore.retrieveUserCLAVersions(req.user["id"])
-        .then(function(claList){
-          console.log(claList)
-          res.render('index', { title: 'CLA Tracker', 'loggedIn':loggedIn, 'profilePicture':profilePicture, 'claList':claList, 'admin':admin});
-      })
+    })
+    .then(function(claListFromDatabase){
+      claList = claListFromDatabase
+      res.render('index', { title: 'CLA Tracker', 'loggedIn':loggedIn, 'profilePicture':profilePicture, 'claList':claList, 'admin':admin});
     })
   //if not logged in render a page with an empty list
   } else {
