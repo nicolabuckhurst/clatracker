02c16153fe0807f57f8397bdd81d6e286f05c2c6
Nicola Buckhurst <nicola@superputerpro.local>
2018-11-12 13:08:41 +0000

    refactored code

--- routes/githublistener.js
+++ routes/githublistener.js
@@ -11,111 +11,107 @@ var Promise = require("bluebird")
 //this route is called when github sends a payload via webhook
 router.post('/', function(req,res,next){
 
-    //CHECK PAYLOAD IS VALID returns a failure object or a success object....see comments below
-    var payloadDataCheck = checkPayload(req);
+  //CHECK PAYLOAD IS VALID returns a failure object or a success object....see comments below
+  var payloadDataCheck = checkPayload(req);
 
-    //deal with invalid payload
-    if(payloadDataCheck["status"]=="failed"){
-      res.status(500).send(payloadDataCheck["message"]);
-      return //return from post function
+  //deal with invalid payload
+  if(payloadDataCheck["status"]=="failed"){
+    res.status(500).send(payloadDataCheck["message"]);
+    console.log(payloadDataCheck["message"])
+    return //return from post function
+  }
+
+  //if raw payload is valid then we create an object with just the relevant data in it
+  var payloadData = payloadDataCheck["data"];
+
+  //CHECK IF AUTHOR IS MEMBER OF ORG returns a failure object or success object......see comments below
+  var isAuthorNonMemberCheck = checkisAuthorNonMember(payloadData);
+
+  //deal with user is a member --send a 200 as this is a valid payload but it doesn't
+  //require us to do anything
+  if(isAuthorNonMemberCheck["status"]=="failed"){
+    res.status(200).send(isAuthorNonMemberCheck["message"]);
+    return //return from post function
+  }
+
+  //return a promise to retrieve the CLARequirements from Database
+  var requiredCLAPromise = databaseStore.retrieveCLARequirementsAsync(payloadData["repoName"])
+  return requiredCLAPromise
+  //then check to see if user has signed required CLA
+  .then(function(version){
+    if(version != null){
+      //if a CLA is required check if user has signed it..returns true or false
+      return databaseStore.checkCLASignedAsync(payloadData["id"], version)
+    } else {
+      //if no CLA is required return "not required" as signed status
+      return "not required"
     }
+  })
+  //then update pullrequest status based on the signed status
+  .then(function(signed){
+    let state
+    let description
+    let target_url
+    let context = "CLATracker"
+    let success_code
+    let success_message
+    let fail_code = 500
+    let fail_message = "something went wrong setting pull request status"
+    
+    requiredCLA = requiredCLAPromise.value() //the variable version is nolonger in scope here but as the promise to get clarequirements has top have resoved here we can just take its value syncronously
+      
+    switch(signed){
+      //if user has signed send a success response and send 201 response
+      case true:
+        console.log("user has signed relevant CLA")
+        state = "success"
+        description = "User has signed the relevant CLA version ( "+ requiredCLA +" )"
+        target_url = "https://localhost:3000"
+        success_code = 201
+        success_message = "User has signed relevant CLA"
+      break;
 
-    //if raw payload is valid then we have an object with just the relevant data in it
-    var payloadData = payloadDataCheck["data"];
+      // if user needs to sign CLA
+      case false:
+        console.log("user has NOT signed relevant CLA")
+        state = "failure"
+        description = "User must sign CLA "+ requiredCLA + " before this pull request can be merged"
+        target_url = "https://localhost:3000/CLA/" + encodeURIComponent(requiredCLA) + "/" + encodeURIComponent(payloadData["repoName"]) + "/" + encodeURIComponent(payloadData["pullRequestSha"]),
+        success_code = 202
+        success_message = "user has NOT signed relevant CLA"
+      break;
 
-    //CHECK IF AUTHOR IS MEMBER OF ORG returns a failure object or success object......see comments below
-    var isAuthorNonMemberCheck = checkisAuthorNonMember(payloadData);
+      //if no CLA is required 
+      case "not required":
+        console.log("CLA not required")
+        state = "success"
+        description = "No CLA required"
+        target_url = "https://localhost:3000"
+        success_code = 203
+        success_message = "CLA Not Required"
+      break;
 
-    //deal with user is a member --send a 200 as this is a valid payload but it doesn't
-    //require us to do anything
-    if(isAuthorNonMemberCheck["status"]=="failed"){
-      res.status(200).send(isAuthorNonMemberCheck["message"]);
-      return //return from post function
+      default:
+          console.log("something went wrong checking CLA signed status")
+          res.status(500).send("unexpected output from checking whether CLA was signed")
     }
+      
 
-    //deal with user is not a member
-    //create 2 seperate promises....to get CLA requirements, and then to check if user
-    //has signed....the second can only resolve once the first has resolved
-    var promiseToReturnCLARequirements = databaseStore.retrieveCLARequirementsAsync(payloadData["repoName"]);
-
-    var promiseToCheckIfSigned = promiseToReturnCLARequirements.then(function(version){
-      //take the Required CLA version or null return and check if user has signed it
-      if(version != null){
-        //if a CLA is required check if user has signed it..returns true or false
-        return databaseStore.checkCLASignedAsync(payloadData["id"], version)
+    return githubInterface.setPullRequestStatusAsync(payloadData["repoName"], payloadData["pullRequestSha"],
+                                           { "state":state,
+                                              "description":description,
+                                              "target_url":target_url,
+                                              "context":context
+                                            },
+                                            process.env.GITHUB_PERSONAL_ACCESS_TOKEN)
+    .then(function(response){
+      if(response == "status set"){
+        res.status(success_code).send(success_message)
       } else {
-        //if no CLA is required return "not required" as signed status
-        return "not required"
+          res.status(fail_code).send(fail_message)
       }
-    })
-
-    //use Promise.join bluebird function to define a function that takes the result
-    //from both resolved promises and execute given function....Do it this way rather
-    //than a single promise chain as we need the version and the signed result to
-    //be in scope at the end of the promise chain
-    return Promise.join(promiseToReturnCLARequirements, promiseToCheckIfSigned,
-      function(requiredCLA, signed){
-        switch(signed){
-          //if user has signed send a success response and send 201 response
-          case true:
-            console.log("user has signed relevant CLA")
-            return githubInterface.setPullRequestStatusAsync(payloadData["repoName"], payloadData["pullRequestSha"],
-                                        { "state":"success",
-                                          "description":"User has signed the relevant CLA version ( "+ requiredCLA +" )",
-                                          "target_url":"https://localhost:3000",
-                                          "context":"CLATracker"
-                                        },
-                                        process.env.GITHUB_PERSONAL_ACCESS_TOKEN)
-              .then(function(response){
-                if(response == "status set"){
-                  res.status(201).send("user has signed relevant CLA")
-                } else {
-                  res.status(500).send("something went wrong setting pull request status")
-                }
-              })
-            break;
-          // if user needs to sign CLA send success response and then set github status as failed
-          case false:
-            console.log("user has NOT signed relevant CLA")
-            return githubInterface.setPullRequestStatusAsync(payloadData["repoName"], payloadData["pullRequestSha"],
-                                        { "state":"failure",
-                                          "description":"User must sign CLA "+ requiredCLA + " before this pull request can be merged",
-                                          //if you want to include spaces and / in url parameters...need to encode them
-                                          "target_url":"https://localhost:3000/CLA/" + encodeURIComponent(requiredCLA) + "/" + encodeURIComponent(payloadData["repoName"]) + "/" + encodeURIComponent(payloadData["pullRequestSha"]),
-                                          "context":"CLATracker"
-                                        },
-                                        process.env.GITHUB_PERSONAL_ACCESS_TOKEN)
-                .then(function(response){
-                  if(response == "status set"){
-                    res.status(202).send("user has NOT signed relevant CLA")
-                  } else {
-                    res.status(500).send("something went wrong setting pull request status")
-                  }
-                })
-            break;
-          //if no CLA is required send a success response and do nothing
-          case "not required":
-            console.log("CLA not required")
-            return githubInterface.setPullRequestStatusAsync(payloadData["repoName"],payloadData["pullRequestSha"],
-                                        { "state":"success",
-                                          "description":"No CLA required",
-                                          "context":"CLATracker"
-                                        },
-                                        process.env.GITHUB_PERSONAL_ACCESS_TOKEN)
-                .then(function(response){
-                  if(response == "status set"){
-                    res.status(203).send("CLA not required")
-                  } else {
-                    res.status(500).send("something went wrong setting pull request status")
-                  }
-                })
-            break;
-          //if checking CLA signed status returns an error
-          default:
-            console.log("something went wrong checking CLA signed status")
-            res.status(500).send("unexpected output from checking whether CLA was signed")
-          }
-      })
+    })  
+  })
 })
 
 //function that checks that payload is valid
